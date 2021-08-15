15eb26b47d30b0bee0dcf12bf4f22c47e9d7f387
Johan Thoren <johan@thoren.xyz>
2021-08-15 13:49:04 +0200

    0.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyz.thoren/luminary "0.5.1-SNAPSHOT"
+(defproject xyz.thoren/luminary "0.6.0-SNAPSHOT"
   :description "Calculate dates based on the Bible and the 1st Book of Enoch."
   :url "https://github.com/johanthoren/luminary"
   :license {:name "LGPL-3.0"
